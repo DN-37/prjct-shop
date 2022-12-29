46b55f2ea2cdc3ae1c9a788ab11296bdff3f19a7
Danila Nefedov <dn375@mail.ru>
2022-12-29 21:08:53 +0400

    created page FullPizza and added hook useParams

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Header from './components/Header';
 import { Home } from './pages/Home';
 import NotFound from './pages/NotFound';
 import Cart from './pages/Cart';
+import FullPizza from './pages/FullPizza';
 
 import { Routes, Route } from 'react-router-dom';
 
@@ -17,7 +18,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="*" element={<NotFound />} />
-          <Route path="/cart" element={<Cart />} />
+          <Route path="/pizza/:id" element={<Cart />} />
+          <Route path="/cart" element={<FullPizza />} />
         </Routes>
       </div>
     </div>
---
+++ src/pages/FullPizza.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const FullPizza = () => {
+    return (
+        <div className="container">
+            <img src='' />
+            <h2>AAAAAAA</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis assumenda facere, consectetur explicabo laborum nemo impedit ipsa molestias quidem asperiores iste laboriosam enim quasi. Aspernatur, fuga impedit. Quas, facilis laboriosam.</p>
+            <h4>250 P</h4>
+
+        </div>
+    )
+}
+
+export default FullPizza;
