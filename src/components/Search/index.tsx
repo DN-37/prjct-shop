21f41d2d62c5bff15c8a75b311953c1196c74e8e
Danila Nefedov <dn375@mail.ru>
2023-01-15 20:51:34 +0400

    rewrited useCallback on useMemo

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -14,10 +14,10 @@ const Search: React.FC = () => {
         inputRef.current?.focus();
     };
 
-    const updateSearchValue = React.useCallback(
-        debounce((str) => {
+    const updateSearchValue = React.useMemo(() =>
+        debounce((str: string) => {
             dispatch(setSearchValue(str));
-        }, 500), [],
+        }, 500), [dispatch]
     );
 
     const ChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -35,29 +35,29 @@ const Search: React.FC = () => {
                 viewBox="0 0 32 32"
                 xmlns="http://www.w3.org/2000/svg">
                 <circle
-                cx="14"
-                cy="14"
-                fill="none"
-                id="XMLID_42_"
-                r="9"
-                stroke="#000000"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeMiterlimit="10"
-                strokeWidth="2"
+                    cx="14"
+                    cy="14"
+                    fill="none"
+                    id="XMLID_42_"
+                    r="9"
+                    stroke="#000000"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeMiterlimit="10"
+                    strokeWidth="2"
                 />
                 <line
-                fill="none"
-                id="XMLID_44_"
-                stroke="#000000"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeMiterlimit="10"
-                strokeWidth="2"
-                x1="27"
-                x2="20.366"
-                y1="27"
-                y2="20.366"
+                    fill="none"
+                    id="XMLID_44_"
+                    stroke="#000000"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeMiterlimit="10"
+                    strokeWidth="2"
+                    x1="27"
+                    x2="20.366"
+                    y1="27"
+                    y2="20.366"
                 />
             </svg>
             <input
